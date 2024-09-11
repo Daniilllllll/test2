788acf401907231f3578d910992c386109fd8530
Daniilllllll <privetdanilka22@gmail.com>
2024-09-11 19:48:01 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 def print_hi (name):
 dfghdfgjdgjgffffffffffffffffffffffffdddddddddddddddddddddddddddddddddddd
+hgfjshgklahgak'dfjh'A;SLFJWOPEFLA;SKFa
